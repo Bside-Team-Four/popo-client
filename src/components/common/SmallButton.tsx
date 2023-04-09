47df4e1cdb6ced3bd47e1ee6aae1c9d698e42f4a
JaeMeDev <jaeme0406@gmail.com>
2023-04-09 15:23:20 +0900

    feat: add common small button

---
+++ src/components/common/SmallButton.tsx
@@ -0,0 +1,19 @@
+import styled from 'styled-components';
+
+const SmallButton = styled.button.attrs({ type: 'button' })`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 71px;
+  height: 36px;
+  font-size: 14px;
+  font-weight: 500;
+  line-height: 20;
+  border-radius: 10px;
+  box-shadow: none;
+  border: 1px solid ${({ theme }) => theme.color.primary};
+  background-color: ${({ theme }) => theme.color.componentBackground.bg01};
+  color: ${({ theme }) => theme.color.primary};
+`;
+
+export default SmallButton;
