dfcafd130f6807f7dd9989bba4eebb86447ab5b4
thxtome <53218264+thxtome@users.noreply.github.com>
2023-11-12 21:59:39 +0900

    feat: profile icon variant
    알림에서 쓰는 둥근 모양, 친구 찾기에서 쓰는 네모 모양으로 종류 추가

--- src/components/common/ProfileIcon.tsx
+++ src/components/common/ProfileIcon.tsx
@@ -1,32 +1,41 @@
 import Image from 'next/image';
 
 import styled from 'styled-components';
+import { useDarkMode } from 'usehooks-ts';
 
 type ProfileImgProps = {
-  gender: string;
+  gender: 'MALE' | 'FEMALE';
+  variant: 'circle' | 'square'
+  img?: string;
 };
 
-export default function ProfileIcon({ gender }: ProfileImgProps) {
+export default function ProfileIcon({ gender, variant, img }: ProfileImgProps) {
+  const { isDarkMode } = useDarkMode();
+  const $size = variant === 'circle' ? 30 : 44;
+  const $borderRadius = variant === 'circle' ? '50%' : '12px';
+  const src = img ?? `/images/${isDarkMode ? 'black' : 'light'}-${gender.toLowerCase()}-icon.svg`;
+
   return (
-    <ImgContainer>
+    <ImgContainer $size={$size} $borderRadius={$borderRadius}>
       <ProfileImg
-        src={`/images/profile-${gender}.svg`}
+        src={src}
         alt="user profile picture"
-        width={30}
-        height={30}
+        width={$size}
+        height={$size}
       />
     </ImgContainer>
   );
 }
 
-const ImgContainer = styled.div`
+const ImgContainer = styled.div<{ $size:number, $borderRadius:string }>`
   display: flex;
   justify-content: center;
   align-items: center;
-  width: 30px;
-  height: 30px;
+  flex-shrink: 0;
+  width: ${({ $size }) => `${$size}px;`}
+  height:  ${({ $size }) => `${$size}px;`}
   background-color: ${(props) => props.theme.color.componentBackground.bg03};
-  border-radius: 50%;
+  border-radius: ${({ $borderRadius }) => `${$borderRadius};`};
   overflow: hidden;
 `;
 
